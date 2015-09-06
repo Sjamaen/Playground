8448aed40fc03fa43166b76142d66d1d587907bb
unknown <Sjamaen>
2015-09-06 22:26:07 +0200

    super secret message

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-alert("Papa Satan En De Verloren WietGrinder! Koop hem nu!"); 
+###ALS JE DIT LEEST.....
+Is het uploaden makkelijk gelukt, zonder papier. Kijk uit naar leuke scripts in de toekomst.. Salud & thnx
+voor het laten begrijpen van github.
+    ¦?_ ?_ ?_ ?_ ?_(? ?¯?+¯?+?¯ (?8?-?
