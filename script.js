e234c3e5fa253e9fcb67be9c6a3a58414ad6d38b
unknown <Sjamaen>
2015-09-06 19:11:00 +0200

    nieuwe javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var result = '';
+
+window.onload = function(){
+  var divResult = document.getElementById('divResult');
+  document.getElementById('bnt1').addEventListener('click', function(){
+      var result = document.getElementById('txtVeld1').value;
+      divResult.innerHTML = "Het resultaat is " + result;
+  }, false)
+}
