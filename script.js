9a9cba994593737a71fbc79a79e8ee141fad9b2a
unknown <Sjamaen>
2015-09-06 21:55:40 +0200

    kijken of het uit mijn hoofd lukt

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-
-var result = '';
-
-window.onload = function(){
-  var divResult = document.getElementById('divResult');
-  document.getElementById('bnt1').addEventListener('click', function(){
-      var result = document.getElementById('txtVeld1').value;
-      divResult.innerHTML = "Het resultaat is " + result;
-  }, false)
-}
-
-alert("Byebye");
+alert("Papa Satan En De Verloren WietGrinder! Koop hem nu!"); 
